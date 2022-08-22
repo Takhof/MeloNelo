b95133dc91c0aab7bfcba3f3a5a47ad2ea8f3692
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-22 18:14:06 +0900

    feat: Test Heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const app = require("./app");
 const PORT = process.env.PORT || 9000;
 const path = require("path");
+const cors = require("cors");
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(cors());
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 app.listen(PORT, () => console.log(`listening on port : ${PORT}`));
