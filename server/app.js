8182e23da325dc4323fbf48493267de2e0378430
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-22 16:25:50 +0900

    Feat: Send Frontpage on no endpath

--- server/app.js
+++ server/app.js
@@ -7,13 +7,17 @@ app.use(express.json());
 app.get("/api", (req, res) => {
   res.send("We did it!");
 });
-//awef
+
 app.get("/api/test", (req, res) => {
   database("performance")
     .select()
     .then((result) => {
       res.send(result);
     });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 module.exports = app;
