211ac6a70582cff28335e1967ff87a2d113cb14a
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-22 18:18:47 +0900

    test: Heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 const app = require("./app");
 const PORT = process.env.PORT || 9000;
 const path = require("path");
-const cors = require("cors");
-
-app.use(cors());
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/build/index.html"));
