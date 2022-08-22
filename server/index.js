87aab43c5e86650a7284a296ca0d6cdb114d9950
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-22 18:50:00 +0900

    feat: Working local heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 9000;
 const path = require("path");
 const express = require("express");
 
-app.use(express.static(path.join(__dirname, "../build/index.html")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
