085423f6c04f7e0044e3a3508b455e432c181517
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-22 18:06:39 +0900

    Feat: Test Heroku

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 const app = require("./app");
 const PORT = process.env.PORT || 9000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 
 app.listen(PORT, () => console.log(`listening on port : ${PORT}`));
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,25 +2,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/",
+    "/api",
     createProxyMiddleware({
       target: "http://localhost:9000",
       changeOrigin: true,
     })
-  ),
-    app.use(
-      "/api",
-      createProxyMiddleware({
-        target: "http://localhost:9000",
-        changeOrigin: true,
-      })
-    );
-
-    app.use(
-      "/api/test",
-      createProxyMiddleware({
-        target: "http://localhost:9000",
-        changeOrigin: true,
-      })
-    ),
+  );
 };
