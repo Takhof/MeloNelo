b043419c89331930d40323ce6f39e994e38d990a
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-22 16:22:55 +0900

    Feat: Test frontpage

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to MeloNelo</h2>
+      </header>
+    </div>
     {/* <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
